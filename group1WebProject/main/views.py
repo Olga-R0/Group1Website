98ef01b618998ea7a2956976388a543c1e96af5b
Olga_Ro <olga77ro@yandex.ru>
2024-04-07 15:02:50 +0300

    Проверка работы "Commit and Push"

--- group1WebProject/main/views.py
+++ group1WebProject/main/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'main/index.html')
 def about(request):
-    return HttpResponse("<h1>about</h1>")
+    return HttpResponse("<h1>about_</h1>")
