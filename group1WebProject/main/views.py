cdc373056a435a0a2339383ca86318e16d91ee9e
Olga_Ro <olga77ro@yandex.ru>
2024-04-26 18:58:09 +0300

    Добавлена главная страница, страница входа. Рефакторинг

--- group1WebProject/main/views.py
+++ group1WebProject/main/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-def index(request):
-    return render(request, 'main/index.html')
+
+def main(request):
+    return render(request, 'main/main.html')
+def singin(request):
+    return render(request, 'main/singin.html')
 def page2(request):
     return render(request, 'main/page2.html')
 def page3(request):
     return render(request, 'main/page3.html')
+def page4(request):
+    return render(request, 'main/page4.html')
 def about(request):
     return HttpResponse("<h1>about</h1>")
